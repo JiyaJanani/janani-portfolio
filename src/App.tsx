a8274ff3d9596fbcdbfe9eca8240e7018925aab7
TWJanani <s.janani@thoughtworks.com>
2024-03-19 18:08:22 +0530

    removing hamburger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { NavBar } from './NavBar';
-import ProfileImageSection from './components/ProfileImageSection/ProfileImageSection';
-import IntroSection from './components/IntroSection/IntroSection';
-import MyTechStack from './components/MyTechStack/MyTechStack';
-import ProjectSection from './components/ProjectSection/ProjectSection';
+import React from "react";
+import "./App.css";
+import ProfileImageSection from "./components/ProfileImageSection/ProfileImageSection";
+import IntroSection from "./components/IntroSection/IntroSection";
+import MyTechStack from "./components/MyTechStack/MyTechStack";
+import ProjectSection from "./components/ProjectSection/ProjectSection";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <NavBar />
-        <ProfileImageSection />
-        <IntroSection />
-        <MyTechStack />
-        <ProjectSection />
-      </header>
+      <div className='nameWrapper'>
+        <div className="name">Janani</div>
+        <div className="name">Senrayaperumal</div>
+      </div>
+
+      <ProfileImageSection />
+      <IntroSection />
+      <MyTechStack />
+      <ProjectSection />
     </div>
   );
 }
